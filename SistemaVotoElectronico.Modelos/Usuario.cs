c5716b463274b4088a399866e40894247df01c59
Andres01110111 <arevalodany16@gmail.com>
2026-01-06 19:55:28 -0500

    Guardando mis cambios de master antes de cambiar a main

--- SistemaVotoElectronico.Modelos/Usuario.cs
+++ SistemaVotoElectronico.Modelos/Usuario.cs
@@ -6,5 +6,11 @@ namespace SistemaVotoElectronico.Modelos
 {
     internal class Usuario
     {
+        public int Id { get; set; } // Llave Primaria
+        public string Nombre { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Rol { get; set; } = "Votante"; // Votante, Administrador, Candidato
+        public bool YaVoto { get; set; } = false;
     }
 }

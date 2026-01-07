9bb8db05c495e3858274decd2d789acca5e2f6c9
pioli <pioli@ANDRES>
2026-01-06 19:55:28 -0500

    Guardando mis cambios de master antes de cambiar a main

--- SistemaVotoElectronico.Modelos/Candidato.cs
+++ SistemaVotoElectronico.Modelos/Candidato.cs
@@ -6,5 +6,12 @@ namespace SistemaVotoElectronico.Modelos
 {
     internal class Candidato
     {
+        public int Id { get; set; } // Llave Primaria
+        public string Nombre { get; set; } = string.Empty;
+        public string Lista { get; set; } = string.Empty;
+
+        // Llave Foránea: Un candidato pertenece a una elección
+        public int EleccionId { get; set; }
+        public Eleccion? Eleccion { get; set; } // Propiedad de navegación
     }
 }
--- SistemaVotoElectronico.Modelos/Eleccion.cs
+++ SistemaVotoElectronico.Modelos/Eleccion.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace SistemaVotoElectronico.Modelos
 {
-    internal class Class1
+    internal class Eleccion
     {
+        public int Id { get; set; } // Llave Primaria
+        public string Nombre { get; set; } = string.Empty;
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public string TipoEleccion { get; set; } = "Nominal"; // Nominal o Plancha
+
     }
 }

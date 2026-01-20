0a55b233751b704678101f68ac04c84981db37b0
Andres01110111 <arevalodany16@gmail.com>
2026-01-20 10:18:29 -0500

    MVC y Creacion de dependencias

---
+++ SistemaVotoElectronico.ApiConsumer/ApiResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVotoElectronico.ApiConsumer
+{
+    internal class ApiResult
+    {
+    }
+}

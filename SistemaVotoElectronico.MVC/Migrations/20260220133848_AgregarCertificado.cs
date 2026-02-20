3874b8083731efcc102f4b2f606fb1a9e5b52084
GabrielCh18 <egchamorrop@utn.edu.ec>
2026-02-20 08:53:57 -0500

    A

---
+++ SistemaVotoElectronico.MVC/Migrations/20260220133848_AgregarCertificado.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaVotoElectronico.MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarCertificado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
